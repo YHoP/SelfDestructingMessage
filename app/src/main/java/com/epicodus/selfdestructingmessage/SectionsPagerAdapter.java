c4bfeb3155f5b77c5a11988e5027093b3622fbd1
YHop <ypeng.tw@gmail.com>
2015-12-17 18:26:16 -0800

    Create SectonsPagerAdapter

---
+++ app/src/main/java/com/epicodus/selfdestructingmessage/SectionsPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.epicodus.selfdestructingmessage;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import com.epicodus.selfdestructingmessage.MainActivity.DummySectionFragment;
+import java.util.Locale;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    protected Context mContext;
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = new DummySectionFragment();
+        Bundle args = new Bundle();
+        args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.title_section1).toUpperCase(l);
+            case 1:
+                return mContext.getString(R.string.title_section2).toUpperCase(l);
+            case 2:
+                return mContext.getString(R.string.title_section3).toUpperCase(l);
+        }
+        return null;
+    }
+}
