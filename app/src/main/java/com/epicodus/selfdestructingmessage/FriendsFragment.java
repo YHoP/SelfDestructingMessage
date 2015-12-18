3d135a289af6791cfae726f21af72c6d5f64c32b
YHop <ypeng.tw@gmail.com>
2015-12-17 19:07:20 -0800

    Add Friends Fragment and it's layout

---
+++ app/src/main/java/com/epicodus/selfdestructingmessage/FriendsFragment.java
@@ -0,0 +1,24 @@
+package com.epicodus.selfdestructingmessage;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FriendsFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friends,
+                container, false);
+
+        return rootView;
+    }
+
+}
